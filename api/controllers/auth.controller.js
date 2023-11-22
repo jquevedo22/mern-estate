731c8ff359b363869f6a250b48d7ac5052ce9e9f
jquevedo22 <jquevedo.tongol@gmail.com>
2023-11-23 00:37:45 +0800

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js';
 import  bcrypt from 'bcrypt';
 import mongoose  from 'mongoose';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
  
 export const signup =  (req, res, next) => {
     const { username, email, password } = req.body;
@@ -29,3 +30,39 @@ export const signup =  (req, res, next) => {
     });
 
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password} = req.body; 
+    try{
+        const validUser = await User.findOne({ email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+
+        const validPassword = bcrypt.compare(password, validUser.password, (err, hash) => {
+            if(err)return next(errorHandler(401,'Auth failed'));
+
+            if(hash){
+                const token = jwt.sign({
+                    email: validUser.email,
+                    userId: validUser._id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: '1h'
+                });
+                return res
+                .cookie('access_token', token , { httpOnly: true })
+                .status(200)
+                .json({
+                    message: 'Auth successful',
+                    token: token,
+                    userId: validUser._id
+                });
+            }
+            next(errorHandler(401, 'Auth failed'));
+        });
+
+    }catch(error){
+        next(error);
+    }
+
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
-
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'; 
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+ 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+   
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(signInStart() );
+    try{
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+      
+
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(signInFailure(data.message));
+        return;
+      } 
+      dispatch(signInSuccess(data));
+      navigate('/');
+
+    }catch(error){
+      dispatch(signInFailure(data.message));
+      
+    }
+
+  }
+
+
   return (
-    <div>SignIn</div>
+    <div className = 'p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+        <form onSubmit={handleSubmit} className = 'flex flex-col gap-4'>
+          <input 
+            type ='email' 
+            placeholder ='email' 
+            className = 'border p-3 rounded-lg' 
+            id = 'email' 
+            onChange={handleChange}
+          />
+
+          <input 
+            type ='password' 
+            placeholder ='password' 
+            className = 'border p-3 rounded-lg' 
+            id = 'password' 
+            onChange={handleChange}
+          />
+
+          <button   
+            className ='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled: opacity-80'>
+            Sign In
+          </button>
+          {error && <p className='text-red-500 mt-5'>{error}</p> }
+        </form>
+
+    </div>
   )
 }
